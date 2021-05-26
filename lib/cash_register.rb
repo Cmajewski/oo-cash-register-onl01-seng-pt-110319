9aa0a564fa2b6f164ae7dfccca9575614a99e715
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-26 17:11:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,7 @@ class CashRegister
     binding.pry
      if @discount
        self.total=self.total*(@discount/100)
+       binding.pry
       "After the discount, the total comes to $#{@total}"
     else
       "There is no discount to apply."
