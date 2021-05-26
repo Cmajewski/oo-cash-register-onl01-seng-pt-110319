bcd7d60c937ceb257cced4d79cc2ae2ea7dd5173
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-26 16:54:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize(employee_discount=nil)
+    @total=0
+    @discount=employee_discount
+  end
 
+  def add_item(title, price,quantity=1)
+    quantity===1?   @total+=price : @total=quantity*price
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
